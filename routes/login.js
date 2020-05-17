5e22dfa739862e1ebcc1c5c688c12b14b8dc7362
Olaf Klöcker <npmokl@gmx.com>
2020-05-17 22:27:24 +0100

    feat: [login] check user credentials against database and store authToken

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,22 @@
 "use strict";
 const debug = require("debug")("tm:login");
+const addSeconds = require('date-fns/addSeconds')
 const config = global.include("config/config");
+const db = global.include("db/db");
+const bcrypt = require("bcrypt");
+const newError = global.include("errors/createError");
+const {createAuthToken} = global.include("util/authToken");
 const { Router } = require("@awaitjs/express");
 // eslint-disable-next-line -- Router starts with uppercase
 const router = Router();
 
-const {loginSchema} = global.include("validations/user");
+const { loginSchema } = global.include("validations/user");
+
+const mismatchError = newError({
+  message: "This combination of user and password is not correct.",
+  code: "USER_OR_PASSWORD_INCORRECT",
+  status: 403
+});
 
 /**
  * Routes for login
@@ -25,8 +36,23 @@ router.postAsync("/login", async (req, res) => {
     errors: { stack: config.isDevelopment }
   });
 
-  // send back "good" data, but remove password
-  res.json({ ...value, password: undefined });
+  const { password: passwordHash, _id: id } = await db.user.findOne({
+    username: value.username
+  });
+  const passwordCorrect = await bcrypt.compare(value.password, passwordHash);
+  if (!passwordCorrect) throw mismatchError;
+
+  const now = new Date();
+  const { token: authToken } = await db.authToken.insert({
+    token: createAuthToken(),
+    userId: id,
+    created: now,
+    ttl: config.authToken.ttl,
+    expires: addSeconds(now, config.authToken.ttl)
+  });
+
+  // send back "good" data
+  res.json({ id, username: value.username, authToken, password: undefined });
 });
 
 module.exports = router;
