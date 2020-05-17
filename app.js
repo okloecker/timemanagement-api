2f65938b4e5da9bdcfae6b6f9f70710eeab2ba73
Olaf Klöcker <npmokl@gmx.com>
2020-05-17 14:02:36 +0100

    fix: [app] move validation errors middleware before general errors

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
+"use strict";
 const debug = require("debug")("tm:app");
 const express = require("express");
 const { addAsync } = require("@awaitjs/express");
 const app = addAsync(express());
 const helmet = require("helmet");
-const validationErrorHandler = require("./errors/validationErrors");
-
-const port = 3001;
-
-app.use(helmet());
-app.use(express.json()); // Used to parse JSON bodies Node.js 4.16+
 
 /* Helper for global absolute require.
  * use with:
  * include('validations/user.js');
  * https://coderwall.com/p/th6ssq/absolute-paths-require
  */
-global.base_dir = __dirname;
-global.abs_path = function(path) {
+global.baseDir = __dirname;
+global.absPath = function(path) {
   // eslint-disable-next-line
-  return base_dir + path;
+  return baseDir + path;
 };
 global.include = function(file) {
   // eslint-disable-next-line
-  return require(abs_path(`/${file}`));
+  return require(absPath(`/${file}`));
 };
 
+const validationErrorHandler = require("./errors/validationErrors");
+const config = require("./config/config");
 const auth = require("./routes/auth");
+const records = require("./routes/records");
+
+// Sends JSON prettified to caller:
+if (config.isDevelopment) app.set("json spaces", 2);
+
+// Clean response headers for security:
+app.use(helmet());
+
+// Used to parse JSON bodies Node.js 4.16+
+app.use(express.json());
+
+// API routes:
 app.use("/auth", auth);
+app.use("/records", records);
 
 /**
  * Log all requests for AAA
@@ -38,8 +48,8 @@ app.postAsync("*", async req => {
 /**
  * Handle unknown GET routes
  */
-app.getAsync("*", async (/*req, res, next*/) => {
-  throw new Error("Unknown route!");
+app.getAsync("*", async req => {
+  throw new Error(`Unknown route ${req.url}`);
 });
 
 /**
@@ -49,17 +59,16 @@ app.postAsync("*", async req => {
   throw new Error(`Unknown route ${req.url}`);
 });
 
+/**
+ * Handle unknown PUT routes
+ */
+app.putAsync("*", async req => {
+  throw new Error(`Unknown route ${req.url}`);
+});
+
 /*
  * Error handling middlewares must come after other routes and middlewares.
  */
-
-/**
- * Log all caught errors.
- */
-app.use((err, req, res, next) => {
-  debug("Caught: %O", err);
-  next(err);
-});
 
 /**
  * Handle ValidationErrors
@@ -67,13 +76,23 @@ app.use((err, req, res, next) => {
 app.use(validationErrorHandler);
 
 /**
+ * Log all caught errors.
+ */
+app.use((err, req, res, next) => {
+  debug("Caught: %O", config.isDevelopment ? err : err.message);
+  next(err);
+});
+
+/**
  * Handle other errors.
  */
 // eslint-disable-next-line -- must include "next" parameter, otherwise Express will not treat this function as error handling middleware
 app.use((err, req, res, next) => {
-  res.status(400).json({ error: { message: err.message } });
+  res.status(400).json({ error: { message: err.message, code: err.code } });
 });
 
-app.listen(port, () =>
-  console.log(`Timemanagement API listening at http://localhost:${port}`)
+app.listen(config.server.port, () =>
+  console.log(
+    `Timemanagement API listening at http://localhost:${config.server.port}`
+  )
 );
