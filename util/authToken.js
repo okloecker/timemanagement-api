9a04391d35d4e430eb6fbfb737838a3fa56aa833
Olaf Klöcker <npmokl@gmx.com>
2020-05-17 22:30:45 +0100

    feat: add authToken creation helper

---
+++ util/authToken.js
@@ -0,0 +1,15 @@
+"use strict";
+const crypto = require("crypto");
+
+const RANDOM_BYTES = 48;
+// due to base64 padding, token length is RANDOM_BYTES * 4/3, a multiple of 4 with max 3 bytes padding
+const AUTH_TOKEN_LEN_MIN = Math.floor(RANDOM_BYTES * 4 / 3);
+const AUTH_TOKEN_LEN_MAX = Math.ceil(RANDOM_BYTES * 4 / 3);
+
+const createAuthToken = _ => crypto.randomBytes(RANDOM_BYTES).toString("base64");
+
+module.exports = {
+  createAuthToken,
+  AUTH_TOKEN_LEN_MIN,
+  AUTH_TOKEN_LEN_MAX
+};
