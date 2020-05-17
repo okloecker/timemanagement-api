b8dbdf9d95f775917da742e023f62bd02ed81410
Olaf Klöcker <npmokl@gmx.com>
2020-05-17 14:01:47 +0100

    feat: [errors] add function to standardize validation errors

--- errors/validationErrors.js
+++ errors/validationErrors.js
@@ -1,51 +1,30 @@
+"use strict";
+const debug = require("debug")("tm:app");
+const config = global.include("config/config");
 const Joi = require("@hapi/joi");
+
+/** Sanitize and standardize validation errors.
+ */
+const convertValidationErrorDetails = details => {
+  return {
+    validation: details.map(detail => ({
+      message: detail.message,
+      path: detail.path,
+      value: detail.context.value
+    }))
+  };
+};
 
 /**
  * Handle ValidationErrors.
- * Will return an object similar to this shape:
- *
-[
-  {
-    "message": "\"username\" length must be at least 3 characters long",
-    "path": ["username"],
-    "type": "string.min",
-    "context": {
-      "limit": 3,
-      "value": "TO",
-      "label": "username",
-      "key": "username"
-    }
-  },
-  {
-    "message": "\"password\" length must be at least 8 characters long",
-    "path": ["password"],
-    "type": "string.min",
-    "context": {
-      "limit": 8,
-      "value": "secret",
-      "label": "password",
-      "key": "password"
-    }
-  },
-  {
-    "message": "\"password\" missing required peer \"repeat_password\"",
-    "path": [],
-    "type": "object.with",
-    "context": {
-      "main": "password",
-      "mainWithLabel": "password",
-      "peer": "repeat_password",
-      "peerWithLabel": "repeat_password",
-      "label": "value",
-      "value": { "username": "TO", "password": "secret" }
-    }
-  }
-]
  */
 const validationErrorHandler = (err, req, res, next) => {
-  if (err instanceof Joi.ValidationError)
-    res.status(400).json({ error: err.details });
-  else next(err);
+  if (err instanceof Joi.ValidationError) {
+    if (config.isDevelopment) debug("ValidationError: %O", err.details);
+    res.status(400).json({
+      error: convertValidationErrorDetails(err.details)
+    });
+  } else next(err);
 };
 
 module.exports = validationErrorHandler;
