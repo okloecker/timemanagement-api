46499bd827fa8ea924e028010b53d1540014f700
Olaf Klöcker <npmokl@gmx.com>
2020-05-17 22:28:50 +0100

    feat: [signup] store user with hashed password in database

--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,14 @@
 "use strict";
 const debug = require("debug")("tm:signup");
+const bcrypt = require("bcrypt");
 const config = global.include("config/config");
 const { Router } = require("@awaitjs/express");
 // eslint-disable-next-line -- Router starts with uppercase
 const router = Router();
+const db = global.include("db/db");
 const { signupSchema } = global.include("validations/user");
+
+const saltRounds = 10;
 
 /*
  * Routes for signup
@@ -18,7 +22,18 @@ router.postAsync("/signup", async (req, res) => {
     abortEarly: false,
     errors: { stack: config.isDevelopment }
   });
+
   debug("called with username %o", username);
+  debug("validation value %O", value);
+
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  const user = await db.user.insert({
+    username: value.username,
+    password: passwordHash
+  });
+  debug("inserted user %o", user);
+
   res.json({ ...value, password: undefined });
 });
 
